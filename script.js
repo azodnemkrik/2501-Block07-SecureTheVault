5152df1edf1a8444f207ba1a879448577b3fb886
Kirk Mendoza <kirkmendoza@gmail.com>
2025-02-12 16:32:19 -0600

    Updated for better comments throughout

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 /*
 Pseudocode:
-1. Create a "vault" feel
-2. Set up variables
-3. Create a message that will be displayed to the user
-4. Enhance the project by adding a little functionality vs. just displaying the message upon loading the page. I will add 2 buttons that will allow the user to either see the combination or opt out of the vault.
+1. Create a "vault" feel in HTML & CSS
+2. Set up appropriate variables in JS
+3. Create message(s) that will be displayed to the user
+4. Enhance the project by adding a little functionality vs. just displaying the message upon loading the page. I will add 2 buttons that will allow the user to either see the combination or opt out of the vault mission.
 */
 
 
-
-// Declare the 3 codes for the combined combination
+// Declare the 3 code numbers to be used for the Combination
 const codeNumber1 = (220 / 2) - 100; // 10
 const codeNumber2 = (1 + 2 + 3 + 4) * 4; // 40 
 const codeNumber3 = codeNumber2 - 1; // 39
-
 
 
 // Declare the Combination to be used for output
@@ -20,12 +18,10 @@ const theCombination = `You have received this message because you have been cho
  ${codeNumber1} - ${codeNumber2} - ${codeNumber3}`;
 
 
-
 // YES Function - user accepted mission, output the Combination
 function outputCombination() {
     alert(theCombination);
 }
-
 
 
 // NO Function - user declines mission, a message from our tactical lead
